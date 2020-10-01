448a1e6719b69b2034e3a4a060bd4d05434d2321
Steven Hofheins <stevenisinsane@gmail.com>
2020-10-01 16:39:15 -0600

    fixed issue where fileupload would grab file extension and put it in the name

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -3,19 +3,19 @@ import S3 from "react-aws-s3";
 
 function Upload() {
   const fileInput = useRef();
-  const handleClick = event => {
+  const handleClick = (event) => {
     event.preventDefault();
     let file = fileInput.current.files[0];
-    let newFileName = fileInput.current.files[0].name;
+    let newFileName = fileInput.current.files[0].name.replace(/\..+$/, "");
     const config = {
-      bucketName: "bucket",
-      dirName: "yourdirectory" /* optional */,
-      region: "us-west-2",
-      accessKeyId: "key",
-      secretAccessKey: "secret"
+      bucketName: process.env.REACT_APP_BUCKET_NAME,
+      dirName: process.env.REACT_APP_DIR_NAME /* optional */,
+      region: process.env.REACT_APP_REGION,
+      accessKeyId: process.env.REACT_APP_ACCESS_ID,
+      secretAccessKey: process.env.REACT_APP_ACCESS_KEY,
     };
     const ReactS3Client = new S3(config);
-    ReactS3Client.uploadFile(file, newFileName).then(data => {
+    ReactS3Client.uploadFile(file, newFileName).then((data) => {
       console.log(data);
       if (data.status === 204) {
         console.log("success");
